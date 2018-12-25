fdbefd018fabd5aa8f3ef99cf60e5cbd1868ccee
Kyohei Oka <0622okakyo@gmail.com>
2018-12-25 17:37:04 +0900

    '練習中'

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express=reqire('express');
 var path=require('path');
+var accounts=require('./route/accounts')
 
 var app=express();
+
+app.set('view engine','jade');
 app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname,'src')));
+app.use('/accounts',accounts);
 
 app.get('/');
 app.get('/')
